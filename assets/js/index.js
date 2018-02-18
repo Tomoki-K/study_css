16d434c1691cfbd34b220590fb511ad2ae6ef3c5
Tomoki Kano <tomofuma@gmail.com>
2018-02-18 22:45:21 +0900

    responsive hamburger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+
+  // scroll section into view
   $("ul#tabs a").click(function(){
     var secNum = $(this).attr("val");
     $(`div#sec${secNum}`)[0].scrollIntoView({
       behavior: "smooth"
     });
+    if ($("ul#tabs").hasClass("showMode")){
+      setTimeout(function() {
+        $("ul#tabs, div#hamburger").toggleClass("showMode");
+      }, 500);
+    }
+  });
+
+  // responsive hamburger menu
+  $("div#hamburger").click(function(){
+    $("ul#tabs, div#hamburger").toggleClass("showMode");
+  });
+  $("div.section").click(function(){
+    if ($("ul#tabs").hasClass("showMode")){
+      $("ul#tabs, div#hamburger").toggleClass("showMode");
+    }
   });
 });
