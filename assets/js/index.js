0ac5d96ea16cfe7195e48ffe182b79ab75a2c5f9
Tomoki Kano <tomofuma@gmail.com>
2018-03-10 22:20:04 +0900

    chaffle fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,8 +49,9 @@ $(document).ready(function() {
     const elements = document.querySelectorAll('[data-chaffle]');
     Array.prototype.forEach.call(elements, function (el) {
         const chaffle = new Chaffle(el, { /* options */ });
-        el.addEventListener('mouseover', function () {
-            chaffle.init();
-        });
+        chaffle.init();
+        // el.addEventListener('mouseover', function () {
+        //     chaffle.init();
+        // });
     });
 });
